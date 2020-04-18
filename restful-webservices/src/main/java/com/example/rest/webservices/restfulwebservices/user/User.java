9b91b3d3cbc08d031e0f16d52662230289d535f8
Ankur Shukla <ashukla@thinkanalytics.com>
2020-04-18 12:49:46 +0530

    create user bean and user dao service

---
+++ restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/user/User.java
@@ -0,0 +1,48 @@
+package com.example.rest.webservices.restfulwebservices.user;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String name;
+    private Date birthdate;
+
+    public User(int id, String name, Date birthdate) {
+        this.id = id;
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthdate=" + birthdate +
+                '}';
+    }
+}
